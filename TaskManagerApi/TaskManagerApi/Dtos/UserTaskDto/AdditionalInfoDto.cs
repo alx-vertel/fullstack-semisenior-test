9ac7f777f28a1659e0f6dbf554d20b026c2c6cb8
Alejandro <alejandro.vertel@doublevpartners.com>
2026-04-16 03:11:10 -0500

    feat: added enum to priority in additional info for UserTask

--- TaskManagerApi/TaskManagerApi/Dtos/UserTaskDto/AdditionalInfoDto.cs
+++ TaskManagerApi/TaskManagerApi/Dtos/UserTaskDto/AdditionalInfoDto.cs
@@ -1,8 +1,21 @@
-﻿namespace TaskManagerApi.Dtos.UserTaskDto
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace TaskManagerApi.Dtos.UserTaskDto
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum UserTaskPriority
+    {
+        Low = 1,
+        Medium = 2,
+        High = 3
+    }
+
     public class AdditionalInfoDto
     {
-        public string Priority { get; set; }
+        [EnumDataType(typeof(UserTaskPriority), ErrorMessage = "Priority is invalid. Allowed values are: Low, Medium, High.")]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public UserTaskPriority? Priority { get; set; } = UserTaskPriority.Medium;
         public DateTime? EstimatedEndDate { get; set; }
         public List<string> Tags { get; set; }
     }
